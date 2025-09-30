7df8da3cb37b752d260227b0c0b3deda0be15541
Adrian Calavie <adrian.calavie06@gmail.com>
2025-09-30 16:19:56 +0300

    move solvable logic into puzzle class

--- src/algorithm/puzzle.ts
+++ src/algorithm/puzzle.ts
@@ -1,10 +1,6 @@
-import type { Direction, Rank } from "../concepts/grid";
+import type { Direction, Position, Rank } from "../concepts/grid";
 import { eq, type Matrix } from "../concepts/matrix";
-import {
-  EMPTY_TILE_VALUE,
-  isEmptyTile,
-  type TileArray,
-} from "../concepts/tile";
+import { EMPTY_TILE_VALUE, type TileArray } from "../concepts/tile";
 import { manhattan } from "./manhattan";
 
 export class PuzzleState {
@@ -48,15 +44,57 @@ export class PuzzleState {
   }
 
   getMoves(): Direction[] {
-    throw new Error("Method not implemented.");
+    const empty = this.emptyTile;
+    const directions: Direction[] = [];
+    if (empty.row < this._rank - 1) directions.push("down");
+    if (empty.row > 0) directions.push("up");
+    if (empty.col < this._rank - 1) directions.push("right");
+    if (empty.col > 0) directions.push("left");
+    return directions;
   }
 
   isSolved(): boolean {
     return eq(this.puzzle, this.solved);
   }
 
+  isSolvable(): boolean {
+    const emptyTileRow = this.puzzle.findIndex((row) => row.includes(0));
+    if (emptyTileRow === -1) {
+      throw new Error("No empty tile found in the puzzle.");
+    }
+
+    const inversions = countInversions(this.puzzle);
+
+    const isEvenRank = this.rank % 2 === 0;
+    const hasEvenInversions = inversions % 2 === 0;
+    const isEmptyTileOnEvenRow = emptyTileRow % 2 === 0;
+
+    if (!isEvenRank) {
+      return hasEvenInversions;
+    }
+    // For even rank grids
+    return (
+      (isEmptyTileOnEvenRow && !hasEvenInversions) ||
+      (!isEmptyTileOnEvenRow && hasEvenInversions)
+    );
+  }
+
   solveAStar(): void {
-    throw new Error("Method not implemented.");
+    if (this.isSolved()) return;
+    if (!this.isSolvable()) {
+      throw new Error("Puzzle is not solvable");
+    }
+  }
+
+  private get emptyTile(): Position {
+    for (let i = 0; i < this._rank; i++) {
+      for (let j = 0; j < this._rank; j++) {
+        if (this.puzzle[i][j] === 0) {
+          return { row: i, col: j };
+        }
+      }
+    }
+    throw new Error("No empty tile found in the puzzle.");
   }
 
   private get solved(): Matrix {
@@ -101,36 +139,17 @@ export const isSolved = (tiles: TileArray, rank: Rank): boolean => {
 };
 
 export const isSolvable = (tiles: TileArray, rank: Rank): boolean => {
-  const emptyTile = tiles.find((tile) => isEmptyTile(tile));
-  if (!emptyTile) {
-    throw new Error("No empty tile found in the puzzle.");
-  }
-
-  const inversions = countInversions(tiles);
-
-  const isEvenRank = rank % 2 === 0;
-  const hasEvenInversions = inversions % 2 === 0;
-  const isEmptyTileOnEvenRow = emptyTile.position.row % 2 === 0;
-
-  if (!isEvenRank) {
-    return hasEvenInversions;
-  }
-
-  // For even rank grids
-  return (
-    (isEmptyTileOnEvenRow && !hasEvenInversions) ||
-    (!isEmptyTileOnEvenRow && hasEvenInversions)
-  );
+  const puzzle = new PuzzleState(tiles, rank);
+  return puzzle.isSolvable();
 };
 
-const countInversions = (tiles: TileArray): number => {
+const countInversions = (matrix: Matrix): number => {
   let inversions = 0;
-  for (let i = 0; i < tiles.length - 1; i++) {
-    for (let j = i + 1; j < tiles.length; j++) {
-      if (isEmptyTile(tiles[i]) || isEmptyTile(tiles[j])) {
-        continue;
-      }
-      if (tiles[i].value > tiles[j].value) {
+  const flat = matrix.flat().filter((value) => value !== 0); // Exclude empty tile
+
+  for (let i = 0; i < flat.length - 1; i++) {
+    for (let j = i + 1; j < flat.length; j++) {
+      if (flat[i] > flat[j]) {
         inversions++;
       }
     }
